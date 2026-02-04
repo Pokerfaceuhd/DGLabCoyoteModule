bf4f64bc0ddb36716579065336442d6fb4744622
Pokerfaceuhd <142628610+Pokerfaceuhd@users.noreply.github.com>
2026-02-04 21:59:13 +0100

    Range change

--- Openshock2Coyote/Config/CoyoteCOnfig.cs
+++ Openshock2Coyote/Config/CoyoteCOnfig.cs
@@ -6,7 +6,7 @@ public sealed class CoyoteConfig
     public bool AutoConnect { get; set; } = false;
     public int DutyCycle { get; set; } = 50;
     public bool Vibrate { get; set; } = true;
-    public JsonRange<float> VibrateMultiplierRange { get; set; } = new() { Min = 0.1f, Max = 1.0f};
-    public JsonRange<float> ShockMultiplierRange { get; set; } = new() { Min = 0.1f, Max = 1.0f };
+    public JsonRange<float> VibrateMultiplierRange { get; set; } = new() { Min = 0.1f, Max = 0.2f};
+    public JsonRange<float> ShockMultiplierRange { get; set; } = new() { Min = 0.05f, Max = 1.0f };
     public BFDirectiveConfig BfDirective { get; set; } = new();
 }
